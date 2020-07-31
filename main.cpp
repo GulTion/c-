d939a511d5c6007488249e77a5a096f0a1de1b53
GulTion <Gkv8192@gmail.com>
2020-07-31 17:17:51 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int ipow(int base, int exp)
+{
+    int result = 1;
+    for (;;)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}
+int main() {
+
+
+
+  printf("%d",65>>1);
+  return 0;
+}
